e5929903b4b8dd1fe35b161987fd700c79d89e7a
Ethan <epaleyron@edenschool.fr>
2023-12-08 13:07:30 +0100

    fin du Menu HTML

---
+++ pages/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+
+<body>
+
+    <h2>Menu</h2>
+    <ul>
+        <li><a href="insertion.php">Insertion d'élèves</a></li>
+        <li><a href="modification.php">Modification d'élèves</a></li>
+        <li><a href="suppression.php">Suppression d'élèves</a></li>
+    </ul>
+
+</body>
+
+</html>
